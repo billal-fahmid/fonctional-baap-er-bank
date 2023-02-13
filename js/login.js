2564f8cf224630ace6e1c9fdbaea58b7d015fc45
billal-fahmid <billalcooom@gmail.com>
2023-02-13 20:26:40 +0600

    update

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ document.getElementById('btn-submit').addEventListener('click' , function(){
     const passwordFeild = document.getElementById('user-password');
     const password = passwordFeild.value ;
     if(email ==='baaper@bank.com' && password === 'secret'){
-        window.location = '/index.html'
+        window.location = '/bank.html'
     }else{
         alert('Email or Password invalid')
     }
